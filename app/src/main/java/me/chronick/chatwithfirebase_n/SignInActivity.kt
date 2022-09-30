7826a01712639de34e2d4ca0f0a55b09bcececc8
ermakovaDev <ermakova.dev@yandex.ru>
2022-09-30 14:28:22 +0300

    reuse icon in mainActivity

--- app/src/main/java/me/chronick/chatwithfirebase_n/SignInActivity.kt
+++ app/src/main/java/me/chronick/chatwithfirebase_n/SignInActivity.kt
@@ -27,7 +27,6 @@ class SignInActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
