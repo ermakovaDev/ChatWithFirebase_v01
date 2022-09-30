fcc84829659b62de3835b0abf001fe48f040b751
ermakovaDev <ermakova.dev@yandex.ru>
2022-09-30 12:29:30 +0300

    add google accoynt authentification

--- app/src/main/java/me/chronick/chatwithfirebase_n/SignInActivity.kt
+++ app/src/main/java/me/chronick/chatwithfirebase_n/SignInActivity.kt
@@ -4,8 +4,11 @@ package me.chronick.chatwithfirebase_n
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -14,13 +17,15 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import me.chronick.chatwithfirebase_n.databinding.ActivitySignInBinding
 class SignInActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySignInBinding
-    lateinit var mGoogleSignInClient: GoogleSignInClient
-    private val Req_Code = 123
-    var firebaseAuth= FirebaseAuth.getInstance()
+    lateinit var launcher: ActivityResultLauncher<Intent>
+    lateinit var auth: FirebaseAuth
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,74 +33,50 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account !=  null){
+                    firebaseAuthWithGoogle(account.idToken!!)
+                }
+            } catch (e: ApiException){
+                Log.d("MyLog", "API Exception")
+            }
+        }
+
+        binding.cardViewSignin.setOnClickListener{
+            signInWithGoogle()
+        }
+
+    }
+
+    private fun getClient(): GoogleSignInClient{
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-// getting the value of gso inside the GoogleSigninClient
-        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
-// initialize the firebaseAuth variable
-        firebaseAuth= FirebaseAuth.getInstance()
-
-        binding.Signin.setOnClickListener{
-            signInGoogle()
-        }
-
-
+        return GoogleSignIn.getClient(this, gso)
     }
 
-    private  fun signInGoogle(){
-
-        val signInIntent:Intent=mGoogleSignInClient.signInIntent
-        startActivityForResult(signInIntent,Req_Code)
+    private fun signInWithGoogle(){
+        val signInClient = getClient()
+        launcher.launch(signInClient.signInIntent)
     }
 
-    // onActivityResult() function : this is where we provide the task and data for the Google Account
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==Req_Code){
-            val task:Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleResult(task)
-        }
-    }
-    // handleResult() function -  this is where we update the UI after Google signin takes place
-    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
-        try {
-            val account: GoogleSignInAccount? =completedTask.getResult(ApiException::class.java)
-            if (account != null) {
-                UpdateUI(account)
-            }
-        } catch (e:ApiException){
-            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show()
-        }
-    }
-    // UpdateUI() function - this is where we specify what UI updation are needed after google signin has taken place.
-    private fun UpdateUI(account: GoogleSignInAccount){
-        val credential= GoogleAuthProvider.getCredential(account.idToken,null)
-        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task->
-            if(task.isSuccessful) {
-                SavedPreference.setEmail(this,account.email.toString())
-                SavedPreference.setUsername(this,account.displayName.toString())
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if (it.isSuccessful){
+                Log.d("MyLog", "Google signIn ok")
+            } else{
+                Log.d("MyLog", "Google signIn error")
             }
         }
     }
-
-    override fun onStart() {
-        super.onStart()
-        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-    }
-
-
-
-
 }
 
 
